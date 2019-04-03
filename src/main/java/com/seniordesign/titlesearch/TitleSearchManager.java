dffba675c1fddfbae97d8529b1ed9c7214e1c8b8
Mina-N <mjn0010@auburn.edu>
2019-04-02 20:05:11 -0500

    updated functions to return ArrayList of WarrantyDeeds

--- src/main/java/com/seniordesign/titlesearch/TitleSearchManager.java
+++ src/main/java/com/seniordesign/titlesearch/TitleSearchManager.java
@@ -1,101 +1,168 @@
 package com.seniordesign.titlesearch;
 
 import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.OutputStream;
+import java.lang.ProcessBuilder;
+import java.util.Scanner;
+import java.nio.file.Paths;
+import com.seniordesign.titlesearch.WarrantyDeed;
 
-import javax.servlet.ServletContext;
+public class TitleSearcherAPI {
+	private static TitleSearcherAPI singleInstance = null;
+	private static ProcessBuilder pb = null;
+	private static Process process;
+	private static Scanner in;
+	private static OutputStream stdin;
+	private String imageDir;
+	private String pythonPath;
+	private String pythonFile;
+	private BufferedInputStream fileContent = null;
 
-public class TitleSearchManager {
-	private static TitleSearchManager singleInstance = null;
-	//private WarrantyDeedManager deedManagerInstance = null;
-	private List<WarrantyDeed> houseHistory;
-	
-	private TitleSearchManager() {
-		houseHistory = new ArrayList<WarrantyDeed>();
+	private TitleSearcherAPI() {
+		//pythonPath = new File("").getAbsolutePath() + File.separator + "apps" + File.separator + "myapp.war" + File.separator + "WEB-INF" + File.separator + "lib" + File.separator + "python.exe";
+		//pythonPath = "/usr/bin/python";
+		pythonPath = "/Users/minanarayanan/anaconda2/bin/python";
+		pythonFile = new File("").getAbsolutePath() + File.separator + "apps";
+		pythonFile += File.separator + "myapp.war" + File.separator + "BeautifulSoupAPI.py";
+		imageDir = new File("").getAbsolutePath() + File.separator + "apps" + File.separator
+		imageDir += "myapp.war" + File.separator + "warrantyDeedPDFs";
+		try {
+			pb = new ProcessBuilder(this.getPythonPath(), this.getPythonFile());
+			pb.redirectErrorStream(true);
+			process = pb.start();
+			in = new Scanner(process.getInputStream());
+			stdin = process.getOutputStream();
+		} catch (IOException exc) {
+			exc.printStackTrace();
+		}
+		System.out.println(pythonPath);
 	}
-	
-	public static TitleSearchManager getInstance() {
+
+	public static TitleSearcherAPI getInstance() {
 		if(singleInstance == null) {
-			singleInstance = new TitleSearchManager();
+			singleInstance = new TitleSearcherAPI();
 		}
 		return singleInstance;
 	}
-	
-	public List<WarrantyDeed> getHouseHistory() {
-		return this.houseHistory;
+
+	public void closeInputStream() {
+		try {
+			this.fileContent.close();
+		} catch (IOException exc) {
+			exc.printStackTrace();
+		}
 	}
-	
-	public void addWarrantyDeedToHouseHistory(WarrantyDeed deed) {
-		this.houseHistory.add(deed);
+
+	private String getImageDirectory() {
+		return this.imageDir;
 	}
-	
-	public WarrantyDeed analyzeTextWithNLU(WarrantyDeed deed) {
-		
-		return deed;
+
+	private String getPythonFile() {
+		return this.pythonFile;
 	}
-	
-	public WarrantyDeed getWarrantyDeed(String bookNo, String pageNo) {
-		WarrantyDeed deed = createWarrantyDeed("WD207-949.pdf");
-		deed.setText("Testing 2");
-		return deed;
+
+	private String getPythonPath() {
+		return this.pythonPath;
 	}
-	
+
+	private BufferedInputStream returnInputStreamOfFile(String fileName) {
+		File downloadedFile = new File(this.getImageDirectory() + File.separator + fileName);
+		if(!downloadedFile.exists()) {
+			System.out.println("File failed to download.");
+			return fileContent;
+		}
+		System.out.println("File downloaded");
+		try {
+			fileContent = new BufferedInputStream(new FileInputStream(downloadedFile));
+		} catch (FileNotFoundException exc) {
+			exc.printStackTrace();
+		}
+		return fileContent;
+	}
+
+	public List<WarrantyDeed> getPDFWarrantyDeed(String firstNameOrBook, String lastNameOrPage, String county, String prePost) {
+		// TODO Change hardcoded state and county values - currently, 0 represents TN
+		//			and 0 represents Humphreys county
+		WarrantyDeed wd = new WarrantyDeed();
+		List<WarrantyDeed> wdList = new ArrayList<WarrantyDeed>();
+		List<String> grantorsList = new ArrayList<String>();
+		List<String> granteesList = new ArrayList<String>();
+		String fileName = "WD" + firstNameOrBook + "-" + lastNameOrPage + ".pdf";
+		String command = "w 0 0 " + this.getImageDirectory() + " pdf " + prePost;
+		int fileCounter = 0;
+		command += " " + firstNameOrBook + " " + lastNameOrPage + " 0\n";
+		try {
+			String line;
+			if(stdin != null) {
+				stdin.write(command.getBytes());
+				stdin.flush();
+				System.out.println(command);
+			}
+			while((in.hasNext()) && (in != null)) {
+				line = in.nextLine();
+				if (line.trim().equals("Date:"))
+				{
+					wd.setTransactionDate(in.nextLine());
+				}
+				if (line.trim().equals("Grantors:"))
+				{
+					line = in.nextLine();
+					while (!(line.trim().equals("Grantees:")))
+					{
+                  if (!(line.trim().isEmpty()))
+                  {
+                     grantorsList.add(line);
+                  }
+						line = in.nextLine();
+					}
+				}
+				if (line.trim().equals("Grantees:"))
+				{
+					line = in.nextLine();
+					granteesList.add(line);
+					while (in.hasNext())
+					{
+						line = in.nextLine();
+						if (!(line.trim().isEmpty()))
+						{
+							granteesList.add(line);
+						}
+					}
+					wd.setGrantors(grantorsList);
+					wd.setGrantees(granteesList);
+					Path pathToFile = Paths.get(this.getImageDirectory() + File.separator + fileName);
+					if (pathToFile.exists()) {
+						try {
+							byte[] buf = Files.readAllBytes(pathToFile);
+							wd.setPDF(buf);
+							wd.setBookNumber(firstNameOrBook);
+							wd.setPageNumber(lastNameOrPage);
+							wdList.add(wd);
+							fileCounter += 1;
+							fileName = "WD" + firstNameOrBook + "-" + lastNameOrPage + "_" + Integer.toString(fileCounter) + ".pdf";
+						} catch (IOException exc) {
+							exc.printStackTrace();
+						}
+					}
+					wd = new WarrantyDeed();
+				}
+			}
+		} catch (IOException exc) {
+			exc.printStackTrace();
+		}
+		return wdList;
+	}
+
+   /*
 	public static void main(String[] args) {
-		TitleSearchManager manager = TitleSearchManager.getInstance();
-		WarrantyDeed deed = manager.createWarrantyDeed("WD207-941.pdf");
-		deed.setText("Book WD207 Page 941 BKIPG: WD207/941-942 19000414 III E VALUE 28900.00 0.00 ???_=- TRANSFERTAX 106.93 = RECORDING FEE 10.00 DPFEE 2.00 REGISTER'S FEE 1.00 ? TOTALAMOUNT 119.93 STATE OF TENNESSEE. HUMPHREYS COUNTY THIS INSTRUMENT WAS PREPARED BY Bankers Title & Escrow Corp. THE ACTUAL CONS DERATION OR VALUE, WHICHEVER 3310 West End Ave., Ste. 540 I I NSFERIs $ 28,900.00. Nashville, TN 37203 Af?ant SUBS D AND SWORN TO BEFORE ME, THIS mi 2019. ary Pu-Iai/ c Maiy MY COMMISSION EXPIRESM P19?25619-LW (AFFIX SEAL) WARRANTY DEED ADDRESS NEW OWNER(S) AS FOLLOWS: SEND TAX BILLS TO: MAP-PARCEL NUMBERS Justin Jake Overman and Kailee Ann NEW OWNER PART OF Overman 1600 SW 78th Ave Apt 1215 MAP 014 PARCEL 024.00 Plantation, FL 33324 (CITY) (STATE) (ZIP) (CITY) (STATE) (ZIP) FOR AND CONSIDERATION OF THE SUM OF TEN DOLLARS, CASH IN HAND PAID BY THE HEREINAFTER NAMED GRANTEES, AND OTHER GOOD AND VALUABLE CONSIDERATIONS. THE RECEIPT OF WHICH IS HEREBY ACKNOWLEDGED, Lakewood Ranches, LLC, a Delaware Limited HEREINAFTER CALLED THE GRANTOR, HAS BARGAINED AND SOLD, AND BY THESE PRESENTS DOES TRANSFER AND CONVEY UNTO Justin Jake Overman and Kailee Ann Overman, husband and wife, ? HEREINAFTER CALLED THE GRANTEES, THEIR HEIRS AND ASSIGNS, A CERTAIN TRACT OR PARCEL OF LAND IN HUMPHREYS COUNTY, STATE OF TENNESSEE, DESCRIBED AS FOLLOWS, TO-WIT: Land situated in the First Civil District of Humphreys County, Tennessee, being Lot No. 156 on the Plan of Lakewood Ranches Subdivision (Phase Four Section 2) \"The Ridgecrest \" of record in Plat Book D, Page 126, in the Register's Of?ce for Humphreys County, Tennessee, to which Plan reference is hereby made for a more complete description of the property. Being a portion of the same property conveyed to Lakewood Ranches, LLC, a Delaware Limited Liability Company by Special Warranty deed from Capital Farm Credit, FLCA, a Federally Chartered Corporation of record in Book WD194, page 2864 Register?s Of?ce for Humphreys County , Tennessee, dated May 17, 2010 and recorded on May 21, 2010. Book WD207 Page 942 THIS CONVEYANCE IS SUBJECT TO: (1) Taxes which have been prorated and assumed by Grantee; (2) All restrictions of record; (3) All easements of record; (4) All visible easements; (5) All matters appearing on the plan of record; (6) All applicable governmental and zoning regulations. This is UNIMPROVED property known as Lot No. 156 Lakewood Ranches. Waverly, TN 37185. TO HAVE AND TO HOLD the said tract or parcel of land, with the appurtenances, estate, title and interest thereto belonging to the said GRANTEES, their heirs and assigns forever; and we do covenant with the said GRANTEES that we are lawfully seized and possessed of said land in fee simple, have a good right to convey it and the same is unencumbered, unless otherwise herein set out; and we do ?lrther covenant and bind ourselves, our heirs and representatives, to warrant and forever defend the title to the said land to the said GRANTEES, their heirs and assigns, against the lawful claims of all persons whomsoever. Wherever used, the singular number shall include the Witness Grantor?s hand this the dav of V 2019. TITLE: AYW 4944/ STATE OF m (M U) COUNTY OF git/MW Before me, the undersigned Notary Public in and for the said County and State, personally appeared D0 ?(UL H?A/i It (MA with whom I am personal] ac uainted (or prove to me n the basis of satisfactory evidence), a?d who, upon oath, acknowledged h self to be the e??i? Emmons Development, LLC, Sole Member of Lakewood Ranches, LLC, the within named bargamor, a limited liability company, and that ihe executed the within instrument for the purposes stated therein by signing the name of Emmomve pment, LLC, as Sole Member of Lakewood Ranches, LLC, by MW self as such gent . Witness my hand and of?cial seal this the day of Fab 2019. My Commission expires: a /Z?I \" I WW??Mt/?b Notary Public? 1/ a ? 1-11th . wMAUSSA LOUISE BEVAN STAFFORD } Notary Public- State 01 Florida ?.?-= a 0?; 5%: Q; : Commisston#FF 206091 9? '32, ?533; My Comm Explres Mar 29 2019 ?? ?1 1?0 / tary Assn. at ?W ?535..? Bonded through National No [2 PGSZAL?WARRANTY DEED STELLA BATCH: 47901 02/11/2019 - 10:48 AM, g ._ MORTGAGE TAX, JANET H. DAVIS REGISTER OF DEEDS STATE OF TENNESSEE COUNTY OF DAVIDSON, 2' Z, Liability Company,, plural, the plural the singular, and the use of any gender shall be applicable to all genders., Lakewood Ranches, LLC BY: Emmons Development, LLC, Its Sole Member BY: MOM WW]");
-		deed.setBookNumber("207");
-		deed.setPageNumber("941");
-		deed.setYearBought("1998");
-		deed.setIsLatest(true);
-		String[] grantors = {"Testing", "Testing 2"};
-		String[] grantees = {"Grantee 1", "Grantee 2"};
-		deed.setGrantors(grantors);
-		deed.setGrantees(grantees);
-		deed.setTransactionDate("2019-02-19");
-		deed.setParentBookNumber("194");
-		deed.setParentPageNumber("2864");
-		manager.addWarrantyDeedToHouseHistory(deed);
-//		WarrantyDeed deed2 = manager.createWarrantyDeed("WD207-949.pdf");
-//		manager.addWarrantyDeedToHouseHistory(deed2);
+		TitleSearcherAPI title = TitleSearcherAPI.getInstance();
+		WarrantyDeed wd = title.getPDFWarrantyDeedfirstNameOrBook("18", "21", "Humphreys", "0");
+		System.out.println(wd.getBookNumber());
 	}
-	
-	public WarrantyDeed createWarrantyDeed(String fileName) {
-		WarrantyDeed deed = null;
-		String fileFolder = new File("").getAbsolutePath() + File.separator + "apps" + File.separator + "myapp.war" + File.separator + "warrantyDeedPDFs";
-		File file = new File(fileFolder  + File.separator + fileName);
-		BufferedInputStream fileContent = null;
-		try {
-			fileContent = new BufferedInputStream(new FileInputStream(file));
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		int bytesToRead;
-		try {
-			bytesToRead = fileContent.available();
-			byte[] pdfInBytes = new byte[bytesToRead];
-			ByteArrayOutputStream stream = new ByteArrayOutputStream();
-			int result;
-			while((result = fileContent.read(pdfInBytes)) != -1) {
-				stream.write(pdfInBytes, 0, result);
-			}
-			if(result == -1) {
-				deed = new WarrantyDeed();
-				deed.setPDF(stream.toByteArray());
-				fileContent.close();
-				stream.close();
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return deed;
-	}
-}
+   */
--- src/main/java/com/seniordesign/titlesearch/WarrantyDeedManager.java
+++ src/main/java/com/seniordesign/titlesearch/WarrantyDeedManager.java
@@ -3,8 +3,8 @@ import java.io.IOException;
 import com.google.gson.Gson;
 
 // Checks the database for the warrantydeed
-// calls mina's class if it doesn't exist in database
-// populates the WarrantyDeed object from the NLUAPI --> GSON
+// Calls Mina's class if it doesn't exist in database
+// Populates the WarrantyDeed object from the NLUAPI --> GSON
 // JRG - last updated 3/26/2019 @ 2:49pm
 
 public class WarrantyDeedManager {
@@ -20,43 +20,47 @@ public class WarrantyDeedManager {
    private String text;
    private byte[] pdf;
 
-	public WarrantyDeed populateWarrantyDeed(String bookNumber, String pageNumber) {
+	public List<WarrantyDeed> populateWarrantyDeed(String bookNumber, String pageNumber) {
 
-		// Calls databaseManager class and (hopefully) retrieves a WarrantyDeed
+		// Calls DatabaseManager class and (hopefully) retrieves a WarrantyDeed
 		DatabaseManagerAPI dbManager = new DatabaseManagerAPI();
-		WarrantyDeed wd = dbManager.getWarrantyDeed(bookNumber,pageNumber);
+		List<WarrantyDeed> wd = new ArrayList<WarrantyDeed>();
+    List<WarrantyDeed> wdFinal = new ArrayList<WarrantyDeed>();
+    wd = dbManager.getWarrantyDeed(bookNumber, pageNumber);
 
-		// If deed doesn't exist within database, Calls TitleSearcher class and retrieves a WarrantyDeed
-		if (wd == null) {
+		// If deed doesn't exist within database, calls TitleSearcher class and retrieves a WarrantyDeed
+		if (wd.size() == 0) {
 			TitleSearcherAPI titleSearch = TitleSearcherAPI.getInstance();
 			// The '0' parameter refers to Humphreys county, and the
 			// '1' field indicates that the time period is before 1993
-			wd = titleSearch.getPDFWarrantyDeedBookNo(bookNumber, pageNumber, '0', '1');
-			if (wd.getPDF().size() == 0) {
+			wd = titleSearch.getPDFWarrantyDeed(bookNumber, pageNumber, '0', '1');
+			if (wd.size() == 0) {
 				// The second '0' parameter indicates that time period is after 1993
-				wd = titleSearch.getPDFWarrantyDeedBookNo(bookNumber, pageNumber, '0', '0');
+				wd = titleSearch.getPDFWarrantyDeed(bookNumber, pageNumber, '0', '0');
+        // Warranty deed is not available on TitleSearcher.com
+        if (wd.size() == 0) {
+          throw new FileNotFoundException("Warranty Deeds were not found.");
+        }
 			}
 		}
 
-		// Warranty deed is not available on TitleSearcher.com
-		if (wd.getPDF().size() == 0) {
-			throw new FileNotFoundException("Warranty Deed was not found.");
-		}
+    for (int counter = 0; counter < wd.size(); counter++)
+    {
+      WarrantyDeed warrantyDeed = wd.get(counter);
+      // Call discovery service to fill WarrantyDeed text field
+      DiscoveryAPI discoveryAPI = new DiscoveryAPI();
+      discoveryAPI.populateWarrantyDeedText(warrantyDeed);
 
-		// Call discovery service to fill WarrantyDeed text field
-		DiscoveryAPI discoveryAPI = new DiscoveryAPI();
-		discoveryAPI.populateWarrantyDeedText(wd);
+      // Calls NLU service to fill entity fields (grantor, grantee)
+      NaturalLanguageUnderstandingAPI nluAPI = new NaturalLanguageUnderstandingAPI();
+      jsonInString = nluAPI.analyzeText(warrantyDeed.getText());
 
-		// Calls nlu service to fill entity fields (grantor, grantee)
-		NaturalLanguageUnderstandingAPI nluAPI = new NaturalLanguageUnderstandingAPI();
-		jsonInString = nluAPI.analyzeText(wd.getText());
-
-		//uses GSON to convert JSON String into Object, uses reflection to store JSON text into WarrantyDeed Object fields
-		Gson gson = new Gson();
-		wd = gson.fromJson(jsonInString, WarrantyDeed.class);
-
-		return wd;
+      // Uses GSON to convert JSON String into Object, uses reflection to store JSON text into WarrantyDeed Object fields
+      Gson gson = new Gson();
+      warrantyDeed = gson.fromJson(jsonInString, WarrantyDeed.class);
+      wdFinal.add(warrantyDeed);
+    }
+		return wdFinal;
 	}
-
 
 }
