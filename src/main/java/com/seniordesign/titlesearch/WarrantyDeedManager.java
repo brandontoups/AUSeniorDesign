7a54eea27b1dc5f4263cb6a7422befabf9ff5163
Justin R Greenfield <justinrgreenfield@me.com>
2019-04-04 16:54:25 -0500

    added populateWarrantyDeed method
    fills in grantors, grantees, bookNumber, and pageNumber

--- src/main/java/com/seniordesign/titlesearch/WarrantyDeedManager.java
+++ src/main/java/com/seniordesign/titlesearch/WarrantyDeedManager.java
@@ -1,15 +1,22 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-import com.google.gson.Gson;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalysisResults;
+
+
 
 // Checks the database for the warrantydeed
-// Calls Mina's class if it doesn't exist in database
-// Populates the WarrantyDeed object from the NLUAPI --> GSON
-// JRG - last updated 3/26/2019 @ 2:49pm
+// calls mina's class if it doesn't exist in database
+// populates the WarrantyDeed object from the NLUAPI --> GSON
+// jrg0041@auburn.edu - last updated 3/26/2019 @ 2:49pm
 
 public class WarrantyDeedManager {
-
-   private String bookNumber, pageNumber;
+	
+   private String[] bookNumber;
+   private String[] pageNumber;
    private String parentBookNumber, parentPageNumber;
    private String[] grantors;
    private String[] grantees;
@@ -19,48 +26,111 @@ public class WarrantyDeedManager {
    private boolean isLatest = false;
    private String text;
    private byte[] pdf;
+   
+    // Populates the WarrantyDeed to auto-fill the UI fields, should be similar for TrustDeed
+    @SuppressWarnings("static-access")
+    public WarrantyDeed populateWarrantyDeed(String bookNumber, String pageNumber) {
 
-	public List<WarrantyDeed> populateWarrantyDeed(String bookNumber, String pageNumber) {
+	// Calls databaseManager class and (hopefully) retrieves a WarrantyDeed
+	DatabaseManagerAPI dbManager = new DatabaseManagerAPI();
+	WarrantyDeed wd = dbManager.getWarrantyDeed(bookNumber,pageNumber);
 
-		// Calls DatabaseManager class and (hopefully) retrieves a WarrantyDeed
-		DatabaseManagerAPI dbManager = new DatabaseManagerAPI();
-		List<WarrantyDeed> wd = new ArrayList<WarrantyDeed>();
-    List<WarrantyDeed> wdFinal = new ArrayList<WarrantyDeed>();
-    wd = dbManager.getWarrantyDeed(bookNumber, pageNumber);
+	// If deed doesn't exist within database, Calls TitleSearcher class and retrieves a WarrantyDeed
+	if (wd == null) {
 
-		// If deed doesn't exist within database, calls TitleSearcher class and retrieves a WarrantyDeed
-		if (wd.size() == 0) {
-			TitleSearcherAPI titleSearch = TitleSearcherAPI.getInstance();
-			// The '0' parameter refers to Humphreys county, and the
-			// '1' field indicates that the time period is before 1993
-			wd = titleSearch.getPDFWarrantyDeed(bookNumber, pageNumber, '0', '1');
-			if (wd.size() == 0) {
-				// The second '0' parameter indicates that time period is after 1993
-				wd = titleSearch.getPDFWarrantyDeed(bookNumber, pageNumber, '0', '0');
-        // Warranty deed is not available on TitleSearcher.com
-        if (wd.size() == 0) {
-          throw new FileNotFoundException("Warranty Deeds were not found.");
-        }
-			}
+		TitleSearcher titleSearch = new TitleSearcher();
+		wd = titleSearch.getPDFWarrantyDeedBookNo(bookNumber, pageNumber);
+
+	}
+
+	// Call discovery service to fill WarrantyDeed text field
+	DiscoveryAPI discoveryAPI = new DiscoveryAPI();
+	discoveryAPI.populateWarrantyDeedText(wd);
+
+	// Calls nlu service to fill entity fields (grantors, grantees, bookNumbers, pageNumbers, etc)
+	NaturalLanguageUnderstandingAPI nluAPI = new NaturalLanguageUnderstandingAPI();
+	@SuppressWarnings("static-access")
+
+	// Populates Deed ------------------------------------------------------------------------------------------------------------>
+	AnalysisResults json = nluAPI.analyzeText(wd.getText());
+
+	int size = json.getEntities().size();
+
+	ArrayList<String> tempGrantors = new ArrayList<String>();			// Using ArrayList for dynamic growth 
+	ArrayList<String> tempGrantees = new ArrayList<String>();			// (Unsure of how many grantors, grantees, etc)
+	ArrayList<String> tempBookNumbers = new ArrayList<String>();		// Holds values for each variable (with possible duplicates)
+	ArrayList<String> tempPageNumbers = new ArrayList<String>();
+
+
+	for (int i = 0; i < size; i++) {									// Stores extracted elements in respective ArrayLists
+
+
+		// Parsed Grantor(s)
+		if (json.getEntities().get(i).getType().equals("Grantor")) {
+
+			tempGrantors.add(json.getEntities().get(i).getText());
+
+
 		}
 
-    for (int counter = 0; counter < wd.size(); counter++)
-    {
-      WarrantyDeed warrantyDeed = wd.get(counter);
-      // Call discovery service to fill WarrantyDeed text field
-      DiscoveryAPI discoveryAPI = new DiscoveryAPI();
-      discoveryAPI.populateWarrantyDeedText(warrantyDeed);
+		// Parsed Grantee(s)
+		if (json.getEntities().get(i).getType().equals("Grantee")) {
 
-      // Calls NLU service to fill entity fields (grantor, grantee)
-      NaturalLanguageUnderstandingAPI nluAPI = new NaturalLanguageUnderstandingAPI();
-      jsonInString = nluAPI.analyzeText(warrantyDeed.getText());
+			tempGrantees.add(json.getEntities().get(i).getText());
 
-      // Uses GSON to convert JSON String into Object, uses reflection to store JSON text into WarrantyDeed Object fields
-      Gson gson = new Gson();
-      warrantyDeed = gson.fromJson(jsonInString, WarrantyDeed.class);
-      wdFinal.add(warrantyDeed);
-    }
-		return wdFinal;
+		}
+
+		// Parsed Book Number
+		if (json.getEntities().get(i).getType().equals("WarrDeedBookNum")) {
+
+			tempBookNumbers.add(json.getEntities().get(i).getText());
+
+		}
+
+		// Parsed Page Number - may need to be int array for overlapping pages
+		if (json.getEntities().get(i).getType().equals("WarrDeedPageNum")) {
+
+			tempPageNumbers.add(json.getEntities().get(i).getText());
+
+		}
+
+
 	}
+
+	//deletes grantor dupes (by type-conversion)
+	Set tempGrantorsSet = new HashSet(tempGrantors);				// converts to HashSet (which doesn't allow duplicates)
+	tempGrantors.clear();											// Clears ArrayList (may be collected by garbage collector...)
+
+	List<String> grantorsND = new ArrayList(tempGrantorsSet);		// Converts to List
+
+	//deletes grantee dupes (by type-conversion)
+	Set tempGranteesSet = new HashSet(tempGrantees);
+	tempGrantees.clear();
+
+	List<String> granteesND = new ArrayList(tempGranteesSet);
+
+	//deletes book number dupes (by type-conversion)
+	Set tempBookNumbersSet = new HashSet(tempBookNumbers);
+	tempBookNumbers.clear();
+
+	List<String> bookNumbersND = new ArrayList(tempBookNumbersSet);
+
+	//deletes page number dupes (by type-conversion)
+	Set tempPageNumbersSet = new HashSet(tempPageNumbers);
+	tempPageNumbers.clear();
+
+	List<String> pageNumbersND = new ArrayList(tempPageNumbersSet);
+
+	// fills arrays for grantors, grantees, bookNumbers, pageNumbers, etc
+	wd.grantors = grantorsND.toArray(new String[grantorsND.size()]);
+	wd.grantees = granteesND.toArray(new String[granteesND.size()]);
+	wd.bookNumber = bookNumbersND.toArray(new String[bookNumbersND.size()]);
+	wd.pageNumber = pageNumbersND.toArray(new String[pageNumbersND.size()]);
+
+
+
+	return wd; 
+	}
+
 
 }
