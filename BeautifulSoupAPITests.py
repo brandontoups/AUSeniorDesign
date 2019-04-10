b4a9951cca998503f8d5016a43c9c88209cd41cb
Mina-N <mjn0010@auburn.edu>
2019-04-10 17:14:20 -0500

    added unit test file

---
+++ BeautifulSoupAPITests.py
@@ -0,0 +1,25 @@
+import unittest
+
+class TitleSearcherTests(unittest.TestCase):
+
+    def test_searchByName(self):
+        pass
+    def test_searchByBookPageNum(self):
+        pass
+    def test_searchOldIndexBooks(self):
+        pass
+    def test_searchNonIndexedDocs(self):
+        pass
+    def test_locateImageURL(self):
+        pass
+    def test_locateDetails(self):
+        pass
+    def test_navigateToSearchPage(self):
+        pass
+    def test_GetWarrantyDeed(self):
+        pass
+    def test_GetTrustDeed(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
